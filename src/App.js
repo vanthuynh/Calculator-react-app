acb705e23ada8a89d2f4241c2c23b24be4d2bbf9
Van Huynh <vthuynh@cpp.edu>
2022-03-02 20:32:18 -0800

    add numClickHandler

--- src/App.js
+++ src/App.js
@@ -19,6 +19,32 @@ const App = () => {
     num: 0,
     res: 0,
   });
+
+  /** numClickHandler get triggered if any number (0-9) was pressed, update the current num value
+   *  Requirements:
+   * - no whole number starts with 0
+   * - no multiple zeros before the comma
+   * - the format will be "0" if "." is pressed first
+   * - number are entered up to 16 integers long
+   */
+   const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    if (calc.num.length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : calc.num % 1 === 0
+            ? Number(calc.num + value)
+            : calc.num + value,
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
   return (
     <Wrapper>
       <Screen value={calc.num ? calc.num : calc.res} />
